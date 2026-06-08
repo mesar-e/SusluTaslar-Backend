03abf048716f22cdfb15479cfe342a999cb29678
mesar-e <mserkanergul@gmail.com>
2026-06-08 23:14:49 +0300

    Revert "User entity'e validasyonlar eklendi"
    This reverts commit 9aff50cb877d5602bd8a8d7a81b0593667156780.

--- api/src/main/java/com/CrystalShop/api/entity/User.java
+++ api/src/main/java/com/CrystalShop/api/entity/User.java
@@ -1,9 +1,6 @@
 package com.CrystalShop.api.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,21 +17,15 @@ public class User {
     @Column(name = "id")
     private Long id;
 
-    @NotBlank(message = "boş bırakılamaz")
     @Column(name = "first_name")
     private String firstName;
 
-    @NotBlank(message = "boş bırakılamaz")
     @Column(name = "last_name")
     private String lastName;
 
-    @NotBlank(message = "E-posta adresi zorunludur")
-    @Email(message = "Lütfen geçerli bir e-posta adresi giriniz")
     @Column(name = "email")
     private String email;
 
-    @NotBlank(message = "Şifre boş bırakılamaz")
-    @Size(min = 6, message = "Şifreniz en az 6 karakter olmalıdır")
     @Column(name = "pass")
     private String password;
 }
