1480b2f5496142ff77aa4c252ab8031d47f6bfcf
mesar-e <mserkanergul@gmail.com>
2026-06-11 19:36:12 +0300

    Category service Dto larla çalışmak için güncellendi

--- api/src/main/java/com/CrystalShop/api/service/CategoryService.java
+++ api/src/main/java/com/CrystalShop/api/service/CategoryService.java
@@ -1,12 +1,17 @@
 package com.CrystalShop.api.service;
 
+import com.CrystalShop.api.dto.CategoryResponse;
+import com.CrystalShop.api.dto.CategoryRequest;
 import com.CrystalShop.api.entity.Category;
 
 import java.util.List;
 
 public interface CategoryService {
-    List<Category> findAll();
+
+    List<CategoryResponse> getAllCategoriesDto();
+    CategoryResponse getCategoryDtoById(Long id);
     Category findById(Long id);
-    Category save(Category category);
-    void delete(Category category);
+    CategoryResponse save(CategoryRequest categoryRequest);
+    CategoryResponse update(Long id, CategoryRequest categoryRequest);
+    void delete(Long id);
 }
--- api/src/main/java/com/CrystalShop/api/service/CategoryServiceImpl.java
+++ api/src/main/java/com/CrystalShop/api/service/CategoryServiceImpl.java
@@ -1,5 +1,7 @@
 package com.CrystalShop.api.service;
 
+import com.CrystalShop.api.dto.CategoryRequest;
+import com.CrystalShop.api.dto.CategoryResponse;
 import com.CrystalShop.api.entity.Category;
 import com.CrystalShop.api.exception.ApiException;
 import com.CrystalShop.api.repository.CategoryRepository;
@@ -7,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -18,27 +20,63 @@ public class CategoryServiceImpl implements CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
+
     @Override
-    public List<Category> findAll() {
-        return categoryRepository.findAll();
+    public List<CategoryResponse> getAllCategoriesDto(){
+        List<Category> categories = categoryRepository.findAll();
+        return categories.stream().map(category -> {
+            CategoryResponse response = new CategoryResponse();
+            response.setId(category.getId());
+            response.setName(category.getName());
+            return response;
+        }).collect(Collectors.toList());
+    }
+
+    @Override
+    public CategoryResponse getCategoryDtoById(Long id) {
+        Category category = findById(id);
+        CategoryResponse response = new CategoryResponse();
+        response.setId(category.getId());
+        response.setName(category.getName());
+        return response;
     }
 
     @Override
     public Category findById(Long id) {
-         Optional<Category> optionalCategory =categoryRepository.findById(id);
-        if (optionalCategory.isPresent()) {
-            return optionalCategory.get();
-        }
-        throw new ApiException("Category is not found with id " + id, HttpStatus.NOT_FOUND);
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new ApiException("Kategori bulunamadı! ID: " + id, HttpStatus.NOT_FOUND));
     }
 
     @Override
-    public Category save(Category category) {
-        return categoryRepository.save(category);
+    public CategoryResponse save (CategoryRequest categoryRequest) {
+        Category category = new Category();
+        category.setName(categoryRequest.getName());
+
+        Category savedCategory = categoryRepository.save(category);
+
+        CategoryResponse response = new CategoryResponse();
+        response.setName(savedCategory.getName());
+        response.setId(savedCategory.getId());
+        return response;
     }
 
     @Override
-    public void delete(Category category) {
+    public CategoryResponse update  (Long id, CategoryRequest categoryRequest) {
+        Category existingCategory = findById(id);
+        existingCategory.setName(categoryRequest.getName());
+
+         Category updatedCAtegory = categoryRepository.save(existingCategory);
+
+         CategoryResponse response = new CategoryResponse();
+         response.setId(updatedCAtegory.getId());
+         response.setName(updatedCAtegory.getName());
+         return response;
+    }
+
+
+    @Override
+    public void delete(Long id) {
+        Category category = findById(id);
         categoryRepository.delete(category);
     }
 }
