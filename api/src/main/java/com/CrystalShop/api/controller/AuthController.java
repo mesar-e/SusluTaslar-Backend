c1bb3fd0f6ad2b4def67b180d4c9fac205be145f
mesar-e <mserkanergul@gmail.com>
2026-06-21 00:47:09 +0300

    authControllera register endpointi eklendi

--- api/src/main/java/com/CrystalShop/api/controller/AuthController.java
+++ api/src/main/java/com/CrystalShop/api/controller/AuthController.java
@@ -2,8 +2,13 @@ package com.CrystalShop.api.controller;
 
 import com.CrystalShop.api.dto.AuthResponse;
 import com.CrystalShop.api.dto.LoginRequest;
+import com.CrystalShop.api.dto.UserRequest;
+import com.CrystalShop.api.dto.UserResponse;
 import com.CrystalShop.api.security.JwtService;
 import com.CrystalShop.api.service.CustomUserDetailsService;
+import com.CrystalShop.api.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -21,11 +26,13 @@ public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final CustomUserDetailsService userDetailsService;
     private final JwtService jwtService;
+    private final UserService userService;
 
-    public AuthController(AuthenticationManager authenticationManager, CustomUserDetailsService userDetailsService, JwtService jwtService) {
+    public AuthController(AuthenticationManager authenticationManager, CustomUserDetailsService userDetailsService, JwtService jwtService, UserService userService) {
         this.authenticationManager = authenticationManager;
         this.userDetailsService = userDetailsService;
         this.jwtService = jwtService;
+        this.userService = userService;
     }
 
     @PostMapping("/login")
@@ -40,5 +47,13 @@ public class AuthController {
         String jwtToken = jwtService.generateToken(user);
 
         return ResponseEntity.ok(new AuthResponse(jwtToken));
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody UserRequest registerRequest) {
+        UserResponse registeredUser = userService.save(registerRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(registeredUser);
     }
 }
