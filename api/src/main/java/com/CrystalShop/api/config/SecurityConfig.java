c267ff6f35780d214b8766596a1eea60ed030d04
mesar-e <mserkanergul@gmail.com>
2026-06-20 23:27:49 +0300

    SecurityConfig frontend için güncellendi

--- api/src/main/java/com/CrystalShop/api/config/SecurityConfig.java
+++ api/src/main/java/com/CrystalShop/api/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/products", "/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
 
